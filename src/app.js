cd7d211a147d1f2942e2678a6a20753d0f51d5d7
Toby Hughes <tobywhughes@gmail.com>
2018-05-28 16:30:16 -0700

    Shell for website

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { Component} from "react";
+import "./app.css";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1> San Diego Mesh Network - Information Coming Soon </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
